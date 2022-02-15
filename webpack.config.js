b22cccc7ad2941e7d01d84b52c8d733f66477697
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-02-15 21:16:56 +0100

    Set up webpack config ant html plugin

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+
+      plugins: [
+        new HtmlWebpackPlugin({
+         title: 'Output Management',
+         title: 'Development',
+        }),
+      ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
